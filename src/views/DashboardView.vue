39edf4b5d9c55da562a084f7d9baeaa2f437863e
Rigel Lago <rigel.lago@gaatu.com>
2024-10-30 22:24:21 +0800

    Update url

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -16,7 +16,7 @@ export default {
   components: { AppHeader, ResponseTable },
   setup() {
     const store = useStore();
-    const socket = io('wss://http-monitor-server.vercel.app');
+    const socket = io('wss://http-monitor-server-production.up.railway.app:3000');
 
     onMounted(() => {
     store.dispatch('fetchResponses');
